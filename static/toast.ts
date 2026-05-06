be71a34908c22965e7a533b8d46bd983ad1a0fb0
mkelvers <mikkelelvers@outlook.com>
2026-05-06 13:36:49 +0200

    chore: apply cannonical tailwind

--- static/toast.ts
+++ static/toast.ts
@@ -8,7 +8,7 @@ const toastContainer = () => {
   if (!container) {
     container = document.createElement('div');
     container.id = 'toast-container';
-    container.className = 'fixed bottom-4 right-4 z-[100] flex flex-col gap-2';
+    container.className = 'fixed bottom-4 right-4 z-100 flex flex-col gap-2';
     document.body.appendChild(container);
   }
   return container;
