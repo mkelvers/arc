3c1215655e8193519f6083d797fba87ea804f0e9
mkelvers <mikkelelvers@outlook.com>
2026-05-06 13:32:06 +0200

    chore: remove console logging

--- static/dedupe.ts
+++ static/dedupe.ts
@@ -1,12 +1,9 @@
 const dedupe = (): void => {
-  console.log('Dedupe running...')
   const seen = new Set<string>()
   const elements = document.querySelectorAll('[data-id]')
-  console.log('Found elements:', elements.length)
   elements.forEach((item) => {
     const id = item.getAttribute('data-id')
     if (id && seen.has(id)) {
-      console.log('Removing duplicate:', id)
       item.remove()
     } else if (id) {
       seen.add(id)
