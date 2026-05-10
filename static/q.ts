b2c9b99ab8fc3df6aad737bde5ea49b80cc3f784
mkelvers <mikkelelvers@outlook.com>
2026-05-10 18:34:02 +0200

    feat: add shared q() qs() DOM helpers

---
+++ static/q.ts
@@ -0,0 +1,8 @@
+export const q = <T extends Element>(container: HTMLElement, selector: string): T | null =>
+  container.querySelector(selector) as T | null
+
+export const qs = <T extends Element>(selector: string): T | null =>
+  document.querySelector(selector) as T | null
+
+export const dataset = (el: HTMLElement, key: string): string =>
+  el.dataset[key] ?? ''
