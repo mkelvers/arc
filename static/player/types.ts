deeeeaaf9f574d6fff345f83eeed9a17634f4cc4
mkelvers <mikkelelvers@outlook.com>
2026-05-10 18:34:05 +0200

    feat: add player types interfaces

---
+++ static/player/types.ts
@@ -0,0 +1,40 @@
+export interface ModeSource {
+  token: string
+  subtitles: SubtitleItem[]
+  qualities?: string[]
+}
+
+export interface SubtitleItem {
+  lang: string
+  token: string
+}
+
+export interface SkipSegment {
+  type: string
+  start: number
+  end: number
+}
+
+export interface SubtitleCue {
+  start: number
+  end: number
+  text: string
+}
+
+export interface SubtitleTrack {
+  lang: string
+  label: string
+  url: string
+}
+
+export interface ActiveSegment {
+  type: string
+  start: number
+  end: number
+}
+
+export interface TimelineBounds {
+  start: number
+  end: number
+  duration: number
+}
