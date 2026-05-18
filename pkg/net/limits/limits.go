60c8f6039b4123b9210437dc4655ff9ea6382d05
mkelvers <mikkelelvers@outlook.com>
2026-05-18 17:49:03 +0200

    chore: formatting

--- pkg/net/limits/limits.go
+++ pkg/net/limits/limits.go
@@ -3,8 +3,7 @@ package limits
 // Common size limits used when reading upstream responses.
 
 const (
-	Bytes512      = 512
-	KiB512  int64 = 512 << 10
-	MiB2    int64 = 2 << 20
+	Bytes512       = 512
+	KiB512   int64 = 512 << 10
+	MiB2     int64 = 2 << 20
 )
-
