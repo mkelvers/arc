54b03f85a22ef95ea861446cab047ab90ee14bfc
mkelvers <mikkelelvers@outlook.com>
2026-05-28 12:52:10 +0200

    refactor: remove CONFLICTS.md and inline avatar URL from migration

--- internal/database/migrations/016_add_avatar_url.sql
+++ internal/database/migrations/016_add_avatar_url.sql
@@ -1,5 +1,3 @@
 -- +goose Up
 ALTER TABLE user ADD COLUMN avatar_url TEXT NOT NULL DEFAULT '';
-
-UPDATE user SET avatar_url = 'https://api.dicebear.com/9.x/dylan/svg?seed=' || username WHERE avatar_url = '';
 -- +goose Down
