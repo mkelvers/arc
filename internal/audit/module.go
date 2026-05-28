c000e7c778157afd078741269a0e97b8abe0d864
mkelvers <mikkelelvers@outlook.com>
2026-05-28 12:39:35 +0200

    refactor: update audit module imports for flattened package structure

--- internal/audit/module.go
+++ internal/audit/module.go
@@ -1,11 +1,9 @@
 package audit
 
 import (
-	"mal/internal/audit/service"
-
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
-	fx.Provide(service.NewAuditService),
+	fx.Provide(NewAuditService),
 )
