cf9c60ba70c7839d30c846138c065cd3273d826a
mkelvers <mikkelelvers@outlook.com>
2026-05-28 12:40:03 +0200

    refactor: update watchlist module imports for flattened package structure

--- internal/watchlist/module.go
+++ internal/watchlist/module.go
@@ -2,21 +2,18 @@ package watchlist
 
 import (
 	"mal/internal/server"
-	"mal/internal/watchlist/handler"
-	"mal/internal/watchlist/repository"
-	"mal/internal/watchlist/service"
 
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
 	fx.Provide(
-		repository.NewWatchlistRepository,
-		service.NewWatchlistService,
-		handler.NewWatchlistHandler,
+		NewWatchlistRepository,
+		NewWatchlistService,
+		NewWatchlistHandler,
 	),
 	fx.Provide(
-		server.AsRouteRegister(func(h *handler.WatchlistHandler) server.RouteRegister {
+		server.AsRouteRegister(func(h *WatchlistHandler) server.RouteRegister {
 			return h
 		}),
 	),
