54708b93a426e387503fb45287e3960a7007dcf9
mkelvers <mikkelelvers@outlook.com>
2026-06-21 02:04:50 +0200

    style: format static/player/source.ts

--- static/player/source.ts
+++ static/player/source.ts
@@ -2,7 +2,9 @@ import { state } from "./state";
 
 export const streamUrlForMode = (mode: string, quality?: string): string => {
   const src = state.playback.modeSources[mode];
-  if (!src?.token) return "";
+  if (!src?.token) {
+    return "";
+  }
 
   let url = `${state.playback.streamURL}?mode=${encodeURIComponent(mode)}&token=${encodeURIComponent(src.token)}`;
   if (src.type === "m3u8") {
