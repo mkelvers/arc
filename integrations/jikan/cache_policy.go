87b67744bea646f86c6580b2888e58112837bff2
mkelvers <mikkelelvers@outlook.com>
2026-06-23 17:09:00 +0200

    refactor: rename constants.go to cache_policy.go

--- integrations/jikan/cache_policy.go
+++ integrations/jikan/cache_policy.go
@@ -1,8 +1,8 @@
-// Package jikan provides a client for the Jikan v4 API.
 package jikan
 
 import "time"
 
+// Cache TTLs used by the Jikan client for endpoint responses.
 const shortCacheTTL = time.Hour     // 1 hour - for frequently changing data
 const longCacheTTL = time.Hour * 24 // 24 hours - for stable data like genres
 const producerCacheTTL = time.Hour * 24 * 30
