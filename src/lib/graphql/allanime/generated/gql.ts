db975c1097f892ac6f8e925090538a56c460a9d4
mkelvers <mikkelelvers@outlook.com>
2026-07-22 00:33:18 +0200

    feat(graphql/allanime): add allanime schema, operations, and generated types

---
+++ src/lib/graphql/allanime/generated/gql.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+import * as types from './graphql';
+
+
+
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
+ */
+type Documents = {
+    "query AllAnimeSearch($search: SearchInput, $translationType: VaildTranslationTypeEnumType) {\n  shows(\n    search: $search\n    limit: 40\n    page: 1\n    translationType: $translationType\n    countryOrigin: ALL\n  ) {\n    edges {\n      _id\n      malId\n      name\n    }\n  }\n}\n\nquery AllAnimeAvailableEpisodes($showId: String!, $start: Float!, $end: Float!) {\n  show(_id: $showId) {\n    availableEpisodesDetail\n  }\n  episodeInfos(showId: $showId, episodeNumStart: $start, episodeNumEnd: $end) {\n    episodeIdNum\n    notes\n  }\n}\n\nquery AllAnimeEpisodeSources($showId: String!, $translationType: VaildTranslationTypeEnumType!, $episodeString: String!) {\n  episode(\n    showId: $showId\n    translationType: $translationType\n    episodeString: $episodeString\n  ) {\n    sourceUrls\n  }\n}": typeof types.AllAnimeSearchDocument,
+};
+const documents: Documents = {
+    "query AllAnimeSearch($search: SearchInput, $translationType: VaildTranslationTypeEnumType) {\n  shows(\n    search: $search\n    limit: 40\n    page: 1\n    translationType: $translationType\n    countryOrigin: ALL\n  ) {\n    edges {\n      _id\n      malId\n      name\n    }\n  }\n}\n\nquery AllAnimeAvailableEpisodes($showId: String!, $start: Float!, $end: Float!) {\n  show(_id: $showId) {\n    availableEpisodesDetail\n  }\n  episodeInfos(showId: $showId, episodeNumStart: $start, episodeNumEnd: $end) {\n    episodeIdNum\n    notes\n  }\n}\n\nquery AllAnimeEpisodeSources($showId: String!, $translationType: VaildTranslationTypeEnumType!, $episodeString: String!) {\n  episode(\n    showId: $showId\n    translationType: $translationType\n    episodeString: $episodeString\n  ) {\n    sourceUrls\n  }\n}": types.AllAnimeSearchDocument,
+};
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query AllAnimeSearch($search: SearchInput, $translationType: VaildTranslationTypeEnumType) {\n  shows(\n    search: $search\n    limit: 40\n    page: 1\n    translationType: $translationType\n    countryOrigin: ALL\n  ) {\n    edges {\n      _id\n      malId\n      name\n    }\n  }\n}\n\nquery AllAnimeAvailableEpisodes($showId: String!, $start: Float!, $end: Float!) {\n  show(_id: $showId) {\n    availableEpisodesDetail\n  }\n  episodeInfos(showId: $showId, episodeNumStart: $start, episodeNumEnd: $end) {\n    episodeIdNum\n    notes\n  }\n}\n\nquery AllAnimeEpisodeSources($showId: String!, $translationType: VaildTranslationTypeEnumType!, $episodeString: String!) {\n  episode(\n    showId: $showId\n    translationType: $translationType\n    episodeString: $episodeString\n  ) {\n    sourceUrls\n  }\n}"): typeof import('./graphql').AllAnimeSearchDocument;
+
+
+export function graphql(source: string) {
+  return (documents as any)[source] ?? {};
+}
---
+++ src/lib/graphql/allanime/generated/graphql.ts
@@ -0,0 +1,104 @@
+/* eslint-disable */
+/** Internal type. DO NOT USE DIRECTLY. */
+type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+/** Internal type. DO NOT USE DIRECTLY. */
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+import type { DocumentTypeDecoration } from '@graphql-typed-document-node/core';
+export type SearchInput = {
+  allowAdult?: boolean | null | undefined;
+  allowUnknown?: boolean | null | undefined;
+  includeTypes?: boolean | null | undefined;
+  query?: string | null | undefined;
+  season?: string | null | undefined;
+  types?: Array<string> | null | undefined;
+};
+
+export type VaildTranslationTypeEnumType =
+  | 'dub'
+  | 'raw'
+  | 'sub';
+
+export type AllAnimeSearchQueryVariables = Exact<{
+  search?: SearchInput | null | undefined;
+  translationType?: VaildTranslationTypeEnumType | null | undefined;
+}>;
+
+
+export type AllAnimeSearchQuery = { shows: { edges: Array<{ _id: string | null, malId: string | null, name: string | null }> | null } | null };
+
+export type AllAnimeAvailableEpisodesQueryVariables = Exact<{
+  showId: string;
+  start: number;
+  end: number;
+}>;
+
+
+export type AllAnimeAvailableEpisodesQuery = { show: { availableEpisodesDetail: unknown } | null, episodeInfos: Array<{ episodeIdNum: number | string | null, notes: string | null }> | null };
+
+export type AllAnimeEpisodeSourcesQueryVariables = Exact<{
+  showId: string;
+  translationType: VaildTranslationTypeEnumType;
+  episodeString: string;
+}>;
+
+
+export type AllAnimeEpisodeSourcesQuery = { episode: { sourceUrls: unknown } | null };
+
+export class TypedDocumentString<TResult, TVariables>
+  extends String
+  implements DocumentTypeDecoration<TResult, TVariables>
+{
+  __apiType?: NonNullable<DocumentTypeDecoration<TResult, TVariables>['__apiType']>;
+  private value: string;
+  public __meta__?: Record<string, any> | undefined;
+
+  constructor(value: string, __meta__?: Record<string, any> | undefined) {
+    super(value);
+    this.value = value;
+    this.__meta__ = __meta__;
+  }
+
+  override toString(): string & DocumentTypeDecoration<TResult, TVariables> {
+    return this.value;
+  }
+}
+
+export const AllAnimeSearchDocument = new TypedDocumentString(`
+    query AllAnimeSearch($search: SearchInput, $translationType: VaildTranslationTypeEnumType) {
+  shows(
+    search: $search
+    limit: 40
+    page: 1
+    translationType: $translationType
+    countryOrigin: ALL
+  ) {
+    edges {
+      _id
+      malId
+      name
+    }
+  }
+}
+    `) as unknown as TypedDocumentString<AllAnimeSearchQuery, AllAnimeSearchQueryVariables>;
+export const AllAnimeAvailableEpisodesDocument = new TypedDocumentString(`
+    query AllAnimeAvailableEpisodes($showId: String!, $start: Float!, $end: Float!) {
+  show(_id: $showId) {
+    availableEpisodesDetail
+  }
+  episodeInfos(showId: $showId, episodeNumStart: $start, episodeNumEnd: $end) {
+    episodeIdNum
+    notes
+  }
+}
+    `) as unknown as TypedDocumentString<AllAnimeAvailableEpisodesQuery, AllAnimeAvailableEpisodesQueryVariables>;
+export const AllAnimeEpisodeSourcesDocument = new TypedDocumentString(`
+    query AllAnimeEpisodeSources($showId: String!, $translationType: VaildTranslationTypeEnumType!, $episodeString: String!) {
+  episode(
+    showId: $showId
+    translationType: $translationType
+    episodeString: $episodeString
+  ) {
+    sourceUrls
+  }
+}
+    `) as unknown as TypedDocumentString<AllAnimeEpisodeSourcesQuery, AllAnimeEpisodeSourcesQueryVariables>;
