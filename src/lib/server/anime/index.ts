302b365df4d9f181fc48443823133acd029d1f9b
mkelvers <mikkelelvers@outlook.com>
2026-07-22 00:34:39 +0200

    feat(server/anime): export allanime and episodes modules

--- src/lib/server/anime/index.ts
+++ src/lib/server/anime/index.ts
@@ -1,9 +1,13 @@
 import { anilist } from './anilist';
+import { allanime } from './allanime';
+import { episodes } from './episodes';
 import { tmdb } from './tmdb';
 
 export const anime = {
     anilist,
+    allanime,
+    episodes,
     tmdb,
 };
 
-export { anilist, tmdb };
+export { allanime, anilist, episodes, tmdb };
