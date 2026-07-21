3b89bacadd42ff53082ea0f31c8d7b98c95db96c
mkelvers <mikkelelvers@outlook.com>
2026-07-21 18:36:51 +0200

    feat(db): add drizzle config and migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+	schema: './src/lib/server/db/schema/index.ts',
+	out: './drizzle',
+	dialect: 'postgresql',
+	dbCredentials: {
+		url: process.env.DATABASE_URL!
+	},
+	strict: true,
+	verbose: true
+});
