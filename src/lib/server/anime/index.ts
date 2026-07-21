f130a9115b95c21c56b94c1adc1acccb91158bae
mkelvers <mikkelelvers@outlook.com>
2026-07-21 18:24:36 +0200

    refactor: export combined anilist and tmdb anime module

--- src/lib/server/anime/index.ts
+++ src/lib/server/anime/index.ts
@@ -1,1 +1,9 @@
-export { anilist } from './anilist';
+import { anilist } from './anilist';
+import { tmdb } from './tmdb';
+
+export const anime = {
+    anilist,
+    tmdb,
+};
+
+export { anilist, tmdb };
