e416161b9182355215bbbae1187c562b94e56f4f
mkelvers <mikkelelvers@outlook.com>
2026-07-28 20:21:25 +0200

    refactor(anime): use locals.user for watchlist

--- src/routes/anime/[id]/+page.server.ts
+++ src/routes/anime/[id]/+page.server.ts
@@ -4,23 +4,20 @@ import { episodeAudioAvailabilityLabel } from '$lib/anime/audio';
 import { anime } from '$lib/server/anime';
 import { toAnimeDetails } from '$lib/server/anime/details';
 import { animeId, loadAnime } from '$lib/server/anime/route';
-import {
-    updateWatchlist,
-    watchlistUser,
-} from '$lib/server/watchlist/action';
+import { updateWatchlist } from '$lib/server/watchlist/action';
 import {
     getWatchlistedAnimeIds,
     getWatchlistState,
 } from '$lib/server/watchlist/store';
 import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, cookies }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
     const id = animeId(params.id);
     if (!id) {
         error(400, 'Invalid anime ID');
     }
 
-    const userId = watchlistUser(cookies);
+    const userId = locals.user?.id;
 
     const result = await loadAnime(id);
 
