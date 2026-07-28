acf4934bb8e8e81f1a04af773f4326821686be94
mkelvers <mikkelelvers@outlook.com>
2026-07-28 21:01:37 +0200

    feat(api): export getWatchlistAnime

--- src/lib/server/anime/anilist/index.ts
+++ src/lib/server/anime/anilist/index.ts
@@ -1,9 +1,11 @@
 import { getAnime } from './details';
 import { getHomepage } from './home';
 import { searchAnime } from './search';
+import { getWatchlistAnime } from './watchlist';
 
 export const anilist = {
     getAnime,
     getHomepage,
+    getWatchlistAnime,
     searchAnime,
 };
