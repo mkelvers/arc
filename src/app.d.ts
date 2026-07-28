861f9174071ba20e221adccf27d2d2a90bdea753
mkelvers <mikkelelvers@outlook.com>
2026-07-28 20:21:15 +0200

    feat(auth): add Locals session and user types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+import type { auth } from '$lib/server/auth';
+
+type AuthSession = typeof auth.$Infer.Session;
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: AuthSession['session'] | null;
+			user: AuthSession['user'] | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
