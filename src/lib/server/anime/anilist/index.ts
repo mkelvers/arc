6798a641d1c73e8912a7fda29a1ac368ecd542da
mkelvers <mikkelelvers@outlook.com>
2026-07-29 14:35:48 +0200

    feat: wire up anilist transfer exports

--- src/lib/server/anime/anilist/index.ts
+++ src/lib/server/anime/anilist/index.ts
@@ -1,11 +1,17 @@
 import { getAnime } from './details';
 import { getHomepage } from './home';
 import { searchAnime } from './search';
+import {
+    getWatchlistTransferAnime,
+    resolveWatchlistImport,
+} from './transfer';
 import { getWatchlistAnime } from './watchlist';
 
 export const anilist = {
     getAnime,
     getHomepage,
+    getWatchlistTransferAnime,
     getWatchlistAnime,
+    resolveWatchlistImport,
     searchAnime,
 };
