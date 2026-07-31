aff76cd61117fa522a5ffc4024ca22f2344f7949
mkelvers <mikkelelvers@outlook.com>
2026-08-01 00:47:23 +0200

    feat: remove watchlist anilist modules

--- src/lib/server/anime/anilist/index.ts
+++ src/lib/server/anime/anilist/index.ts
@@ -1,17 +1,9 @@
 import { getAnime } from './details';
 import { getHomepage } from './home';
 import { searchAnime } from './search';
-import {
-    getWatchlistTransferAnime,
-    resolveWatchlistImport,
-} from './transfer';
-import { getWatchlistAnime } from './watchlist';
 
 export const anilist = {
     getAnime,
     getHomepage,
-    getWatchlistTransferAnime,
-    getWatchlistAnime,
-    resolveWatchlistImport,
     searchAnime,
 };
