227bf0e8026a20200707050e2f44b9c84a7ae1c7
mkelvers <mikkelelvers@outlook.com>
2026-08-07 10:47:09 +0200

    style: format app shell with oxfmt

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from 'better-auth/svelte';
 import { usernameClient } from 'better-auth/client/plugins';
 
 export const authClient = createAuthClient({
-    plugins: [usernameClient()],
+  plugins: [usernameClient()],
 });
