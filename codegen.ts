e2d98636561b0e3165cddfb7fb46789b7997428e
mkelvers <mikkelelvers@outlook.com>
2026-08-11 10:39:00 +0200

    reformat project configs

--- codegen.ts
+++ codegen.ts
@@ -1,44 +1,44 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  generates: {
-    'src/lib/graphql/anilist/generated/': {
-      schema: 'https://graphql.anilist.co',
-      documents: 'src/lib/graphql/anilist/*.graphql',
-      preset: 'client',
-      config: {
-        documentMode: 'string',
-        enumsAsTypes: true,
-        skipTypename: true,
-        useTypeImports: true,
-      },
-    },
-    'src/lib/graphql/allanime/generated/': {
-      schema: 'src/lib/graphql/allanime/schema.graphql',
-      documents: 'src/lib/graphql/allanime/operations/*.graphql',
-      preset: 'client',
-      config: {
-        documentMode: 'string',
-        enumsAsTypes: true,
-        skipTypename: true,
-        useTypeImports: true,
-        scalars: {
-          DateTime: {
-            input: 'string',
-            output: 'string',
-          },
-          FlexibleInt: {
-            input: 'number | string',
-            output: 'number | string',
-          },
-          Object: {
-            input: 'unknown',
-            output: 'unknown',
-          },
+    generates: {
+        'src/lib/graphql/anilist/generated/': {
+            schema: 'https://graphql.anilist.co',
+            documents: 'src/lib/graphql/anilist/*.graphql',
+            preset: 'client',
+            config: {
+                documentMode: 'string',
+                enumsAsTypes: true,
+                skipTypename: true,
+                useTypeImports: true,
+            },
         },
-      },
+        'src/lib/graphql/allanime/generated/': {
+            schema: 'src/lib/graphql/allanime/schema.graphql',
+            documents: 'src/lib/graphql/allanime/operations/*.graphql',
+            preset: 'client',
+            config: {
+                documentMode: 'string',
+                enumsAsTypes: true,
+                skipTypename: true,
+                useTypeImports: true,
+                scalars: {
+                    DateTime: {
+                        input: 'string',
+                        output: 'string',
+                    },
+                    FlexibleInt: {
+                        input: 'number | string',
+                        output: 'number | string',
+                    },
+                    Object: {
+                        input: 'unknown',
+                        output: 'unknown',
+                    },
+                },
+            },
+        },
     },
-  },
 };
 
 export default config;
