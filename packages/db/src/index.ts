0f95c40bbea285f0f3cc86afa4fad70b276ef2ae
mkelvers <mikkelelvers@outlook.com>
2026-08-20 13:11:02 +0200

    format database client

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,12 +4,12 @@ import postgres from 'postgres';
 import * as schema from './schema';
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not configured');
+    throw new Error('DATABASE_URL is not configured');
 }
 
 export const db = drizzle({
-  client: postgres(process.env.DATABASE_URL),
-  schema
+    client: postgres(process.env.DATABASE_URL),
+    schema,
 });
 
 export type DatabaseTransaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
