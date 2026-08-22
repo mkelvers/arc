974a79ed34d6e5161b09b5311f6d1214fb8dad56
mkelvers <mikkelelvers@outlook.com>
2026-08-22 16:18:47 +0200

    refactor: move tmdb foundation to backend package

--- packages/backend/src/anime/tmdb/client.ts
+++ packages/backend/src/anime/tmdb/client.ts
@@ -1,4 +1,3 @@
-import { env } from '$env/dynamic/private';
 import createClient from 'openapi-fetch';
 
 import type { paths } from './generated';
@@ -13,14 +12,14 @@ function timedFetch(request: Request) {
 }
 
 export function create() {
-    if (!env.TMDB_READ_ACCESS_TOKEN) {
+    if (!process.env.TMDB_READ_ACCESS_TOKEN) {
         throw new TypeError('TMDB_READ_ACCESS_TOKEN is required');
     }
 
     return createClient<paths>({
         baseUrl,
         headers: {
-            Authorization: `Bearer ${env.TMDB_READ_ACCESS_TOKEN}`,
+            Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`,
         },
         fetch: timedFetch,
     });
