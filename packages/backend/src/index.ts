4f95349a9123500c6fe82542557df5a1d8b316cf
mkelvers <mikkelelvers@outlook.com>
2026-08-22 16:20:30 +0200

    feat: export backend application operations

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,16 @@
+export {
+    claimInvitation,
+    completeInvitation,
+    hasInvitationClaim,
+    InvalidInvitationError,
+    InvitationCompletionError,
+    registerInvitedAccount,
+    restoreInvitation,
+} from './invitations';
+export {
+    getWatchlistPage,
+    getWatchlistState,
+    getWatchlistStates,
+    removeFromWatchlist,
+    setWatchlistState,
+} from './watchlist/application';
