db066ee46fa5eec582644914ccadcc1847a2eb81
mkelvers <mikkelelvers@outlook.com>
2026-08-25 23:13:23 +0200

    feat: register new anime providers

--- packages/backend/src/anime/providers/index.ts
+++ packages/backend/src/anime/providers/index.ts
@@ -2,11 +2,17 @@ import { allanimeProvider } from './allanime';
 import { anikotoProvider } from './anikoto';
 import { aninekoProvider } from './anineko';
 import { animeggProvider } from './animegg';
+import { aniDBAppProvider } from './anidb-app';
+import { animeDunyaProvider } from './animedunya';
+import { animeNoSubProvider } from './animenosub';
 import { animepaheProvider } from './animepahe';
 import { anipubProvider } from './anipub';
 import { anizoneProvider } from './anizone';
 import { createProviderFallback } from './fallback';
+import { kickAssAnimeProvider } from './kickassanime';
+import { reAnimeProvider } from './reanime';
 import { senshiProvider } from './senshi';
+import { twoDHiveProvider } from './two-d-hive';
 
 export const playbackProviders = [
     allanimeProvider,
@@ -17,6 +23,12 @@ export const playbackProviders = [
     anipubProvider,
     animepaheProvider,
     anizoneProvider,
+    reAnimeProvider,
+    aniDBAppProvider,
+    twoDHiveProvider,
+    animeDunyaProvider,
+    animeNoSubProvider,
+    kickAssAnimeProvider,
 ] as const;
 
 export const playback = createProviderFallback(playbackProviders);
