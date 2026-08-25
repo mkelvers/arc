81cbd7e0d4544aac2676b77ce1047a5a3e5d2041
mkelvers <mikkelelvers@outlook.com>
2026-08-25 18:24:34 +0200

    update WatchlistPageContent component

--- apps/arc/src/routes/(app)/watchlist/_components/WatchlistPageContent.svelte
+++ apps/arc/src/routes/(app)/watchlist/_components/WatchlistPageContent.svelte
@@ -369,7 +369,7 @@
                     />
                 {:else}
                     <div
-                        class="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 2xl:gap-x-5"
+                        class="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-4 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-[1.875rem] lg:gap-y-12 xl:grid-cols-6 2xl:grid-cols-7"
                     >
                         {#each data.entries.filter((entry) => !watchlist.loaded || watchlist.state(entry.id)) as entry (entry.id)}
                             {#if entry.pendingMetadata}
