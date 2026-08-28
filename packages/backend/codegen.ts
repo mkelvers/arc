368ca495652dc1aa640c6a4ec899946012cb0c9a
mkelvers <mikkelelvers@outlook.com>
2026-08-28 20:51:55 +0200

    remove allanime provider

--- packages/backend/codegen.ts
+++ packages/backend/codegen.ts
@@ -13,30 +13,5 @@ export default {
                 useTypeImports: true,
             },
         },
-        'src/anime/allanime/generated/': {
-            schema: 'src/graphql/operations/allanime/schema.graphql',
-            documents: 'src/graphql/operations/allanime/operations/*.graphql',
-            preset: 'client',
-            config: {
-                documentMode: 'string',
-                enumsAsTypes: true,
-                skipTypename: true,
-                useTypeImports: true,
-                scalars: {
-                    DateTime: {
-                        input: 'string',
-                        output: 'string',
-                    },
-                    FlexibleInt: {
-                        input: 'number | string',
-                        output: 'number | string',
-                    },
-                    Object: {
-                        input: 'unknown',
-                        output: 'unknown',
-                    },
-                },
-            },
-        },
     },
 } satisfies CodegenConfig;
