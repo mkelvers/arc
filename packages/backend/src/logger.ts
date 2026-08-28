20fb90159abb779884323c870c04b9eb23bb8e1a
mkelvers <mikkelelvers@outlook.com>
2026-08-28 18:36:28 +0200

    simplify playback debug logger

--- packages/backend/src/logger.ts
+++ packages/backend/src/logger.ts
@@ -7,14 +7,8 @@ export interface Logger {
 }
 
 const debug = debuglog('arc:playback');
-const verbose = process.argv.some((argument) => argument === '-v' || argument === '--verbose');
+const log: DebugLogger = process.argv.includes('-v') ? console.error : debug;
 
 export const logger: Logger = {
-    debug(message, ...values) {
-        if (verbose) {
-            console.error(`ARC:PLAYBACK ${process.pid}:`, message, ...values);
-        } else {
-            debug(message, ...values);
-        }
-    },
+    debug: log,
 };
