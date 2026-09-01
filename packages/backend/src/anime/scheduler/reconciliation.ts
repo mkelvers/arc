fe61ccf79930be793a3f8936a770b107267cfa8c
mkelvers <mikkelelvers@outlook.com>
2026-09-01 11:48:24 +0200

    refactor: drop unused AiringAnime type import

--- packages/backend/src/anime/scheduler/reconciliation.ts
+++ packages/backend/src/anime/scheduler/reconciliation.ts
@@ -2,7 +2,7 @@ import { and, eq, inArray, notInArray } from 'drizzle-orm';
 
 import { db } from '@arc/db';
 import { animeEpisodeSync, animeRelease, animeReleaseRequest } from '@arc/db/schema';
-import { discoverAiringAnime, type AiringAnime } from '../anilist/airing';
+import { discoverAiringAnime } from '../anilist/airing';
 import { airingTargetSchedules } from './airing-policy';
 import { scheduleAiringTargets, scheduleReleaseTargets } from './targets';
 
