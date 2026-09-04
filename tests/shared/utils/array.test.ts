877721f1c2def6ebb9b93dd6c6183c2b36023ab8
mkelvers <mikkelelvers@outlook.com>
2026-09-04 11:08:04 +0200

    refactor: move nullable array utility to shared

---
+++ tests/shared/utils/array.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'bun:test';
+
+import { present } from '@arc/shared/utils/array';
+
+describe('shared array utilities', () => {
+    test('removes null values from nullable arrays', () => {
+        expect(present(['anime', null, 'catalog'])).toEqual(['anime', 'catalog']);
+        expect(present(null)).toEqual([]);
+    });
+});
